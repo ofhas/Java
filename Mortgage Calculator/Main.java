e1e3d435298e2b9a453f7af7c01fd661334d93c4
ofhas <53121171+ofhas@users.noreply.github.com>
2020-02-18 18:23:51 +0200

    Update Main.java

--- Mortgage Calculator/Main.java
+++ Mortgage Calculator/Main.java
@@ -8,18 +8,18 @@ public class Main {
 
     public static void main(String[] args) {
         //Mortgage calculator
-        final int TWELVE = 12;
-        final int HOUNDED = 100;
+        final int NUM_OF_MONTHS = 12;
+        final int PARENTAGE = 100;
         Scanner scanner = new Scanner(System.in);
         System.out.print("Principal: ");
         int principal = scanner.nextInt();
 
         System.out.print("Annual interest rate: ");
-        double annualInterestRate = scanner.nextDouble()/(TWELVE*HOUNDED);
+        double annualInterestRate = scanner.nextDouble()/(NUM_OF_MONTHS*PARENTAGE);
 
         System.out.print("Years: ");
         int years = scanner.nextInt();
-        int numOfPayments = years * 12;
+        int numOfPayments = years * NUM_OF_MONTHS;
 
         double mortgage = principal * ((annualInterestRate*(Math.pow(1+annualInterestRate, numOfPayments))/(Math.pow(1+annualInterestRate, numOfPayments)-1)));
         NumberFormat currency = NumberFormat.getCurrencyInstance();
